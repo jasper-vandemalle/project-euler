b5b533eefcece9b8a18d7b5e7dab43647e9c6827
Jasper Vandemalle <jasper.vandemalle@gmail.com>
2013-05-26 12:36:22 +0200

    Adding solutions for problems 2 and 3
    Solving problems 2 and 3, updating the .gitignore to ignore eclipse
    configuration files. Updating pom to include Guava.

---
+++ src/main/java/be/vandemalle/jasper/project/euler/Problem2.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package be.vandemalle.jasper.project.euler;
+
+/**
+ * Solution to Project Euler problem 2
+ * 
+ * @author Jasper Vandemalle
+ * @see <a href="http://projecteuler.net/problem=2">http://projecteuler.net/problem=2</a>
+ * 
+ */
+public class Problem2 implements Solvable {
+	/** {@inheritDoc} */
+	@Override
+	public String solve() {
+		int sumOfEvenFibonacciTerms = 0;
+		int previousFibonacciTerm = 1;
+		int currentFibonacciTerm = 2;
+
+		while (currentFibonacciTerm < 4000000) {
+			if (currentFibonacciTerm % 2 == 0) {
+				sumOfEvenFibonacciTerms += currentFibonacciTerm;
+			}
+
+			int temp = currentFibonacciTerm;
+			currentFibonacciTerm += previousFibonacciTerm;
+			previousFibonacciTerm = temp;
+		}
+
+		return Integer.toString(sumOfEvenFibonacciTerms);
+	}
+
+	/**
+	 * Run and solve the problem.
+	 * 
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println(new Problem2().solve());
+	}
+}
---
+++ src/main/java/be/vandemalle/jasper/project/euler/Problem3.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package be.vandemalle.jasper.project.euler;
+
+import java.util.List;
+
+import be.vandemalle.jasper.project.euler.utils.PrimeUtils;
+
+/**
+ * Solution to Project Euler problem 3
+ * 
+ * @author Jasper Vandemalle
+ * @see <a href="http://projecteuler.net/problem=3">http://projecteuler.net/problem=3</a>
+ * 
+ */
+public class Problem3 implements Solvable {
+	/** {@inheritDoc} */
+	@Override
+	public String solve() {
+		Long number = 600_851_475_143L;
+		List<Integer> primes = PrimeUtils.sieveOfAtkinAsList(30_000);
+
+		while (!primes.contains(number.intValue())) {
+			for (Integer prime : primes) {
+				if (number % prime == 0) {
+					number = number / prime;
+					break;
+				}
+			}
+		}
+
+		return Long.toString(number);
+	}
+
+	/**
+	 * Run and solve the problem.
+	 * 
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println(new Problem3().solve());
+	}
+}
